657b73d63977a2ccc16a2c76ea5e354862c715ab
Brian <bmaitner@gmail.com>
2022-11-09 14:49:19 -0500

    added figure showing the proportion of papers with available code over time

---
+++ 2_get_paper_info.R
@@ -0,0 +1,40 @@
+# in this script, the goal is to extract information from the manually-populated google sheet
+
+# Data to be gathered includes:
+
+# 1) Number of papers with vs without scripts available over time
+# 2) A google sheet listing the packages used by each of the available scripts.  This will be used to assess correspondence between packages used and cited
+    # if possible, this should include both libraries called and dependencies, and should distinguish between the two
+
+################################################################################
+
+# Load libraries
+library(tidyverse)
+library(googledrive)
+library(googlesheets4)
+
+
+# Pull the current version of hte google sheet
+cite_data <- read_sheet("1pYB_oJt-Sx__OKJdmlgEpBmDFLmGLxh9Rddp9qKNooE")
+
+
+
+cite_data%>%
+  group_by(year)%>%
+  summarise(n_scripts_available = sum(na.omit(r_scripts_available=="yes")),
+            n_scripts_not_available = sum(na.omit(r_scripts_available=="no")),
+            prop_scripts_available = n_scripts_available / (n_scripts_available + n_scripts_not_available))%>%
+  ungroup()%>%
+  mutate(year=as.integer(year))%>%
+  ggplot(aes(x=year,
+             y=prop_scripts_available))+
+  geom_line()+
+  ylab("Proportion of Papers with R Scripts Available")+
+  ylim(c(0,1))+
+  scale_x_continuous(limits = c(2010, 2022), breaks = seq(2010, 2022, 1))
+
+
+
+
+
+
