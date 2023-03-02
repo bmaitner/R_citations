eb9a0c320e7a3b852046b46efe1a34a6239e90b6
Luna <62033805+Luna5ama@users.noreply.github.com>
2023-03-02 17:23:39 -0500

    Google Sheet downloading

---
+++ py/google_sheet.py
@@ -0,0 +1,30 @@
+import requests
+import os
+import csv
+import utils
+
+
+def download():
+    sheet_id = "1pYB_oJt-Sx__OKJdmlgEpBmDFLmGLxh9Rddp9qKNooE"
+
+    response = requests.get(f"https://docs.google.com/spreadsheet/ccc?key={sheet_id}&output=csv")
+    response.raise_for_status()
+
+    with open("run/r_citations_sheet.csv", "w") as f:
+        f.write(response.text)
+
+
+def read():
+    if not os.path.exists("run/r_citations_sheet.csv"):
+        download()
+
+    result = []
+
+    with open("run/r_citations_sheet.csv") as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        for row in reader:
+            result.append(dict(zip(header, row)))
+
+    return result
+
---
+++ py/utils.py
@@ -0,0 +1,7 @@
+import os
+
+def create_if_not_exist(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+create_if_not_exist("run")
